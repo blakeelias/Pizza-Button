b3eb90ae06904b032dfd6869d084a1bae1918c33
Vinay Mayar <vinaymayar@gmail.com>
2013-10-06 08:37:06 -0400

    Change order preferences now redirects to account page
    if one is not set up.  Otherwise, it uses API call
    to find the user's address.

--- src/js/frontend/setUpRestaurantsTable.js
+++ src/js/frontend/setUpRestaurantsTable.js
@@ -1,5 +1,5 @@
-define([],
-    function() {
+define(["cookies/getCookies"],
+    function(getCookies) {
         return function () {
             var loadTable = function(data) {
                 var myData = (typeof data === "string") ? JSON.parse(data) : data;
@@ -9,6 +9,7 @@ define([],
                     "bServerSide":true,
                     "sAjaxSource":"/cgi-bin/restaurantList.py?addr="+data.addr+"&city="+data.city+"&zip="+data.zip,
                     "bSort": true,
+                    "iDisplayLength":5,
                     "aoColumns": [
                         {"bVisible":false},
                         null,
@@ -19,8 +20,28 @@ define([],
                 });
             };
 
-            var mData = {addr: "3 Ames Street",city:"Cambridge",zip:"02142"};
-            loadTable(mData);
+            var cookieInfo = getCookies();
+            var username = cookieInfo.username;
+            var password = cookieInfo.password;
+
+            $.ajax("/cgi-bin/single_address_cgi.py",
+                    {
+                        type : "GET",
+                        data : {
+                            email : username,
+                            current_pw : password,
+                            nick : "address1"
+                        },
+                        contentType : "application/json;charset:utf-8",
+                        success : function(jqXHR) {
+                            var o_jqXHR = (typeof jqXHR === "string") ? JSON.parse(jqXHR) : jqXHR;
+                            loadTable(o_jqXHR);
+                        },
+                        error : function(jqXHR, textStatus, errorThrown) {
+                            $("#dropdown").tabs({active : 0});
+                            error("Please create an account before setting delivery preferences");
+                        }
+                    });
         }
 
 
