f0cf2b84f8568d44dbb80d239a64d1e9bb332704
demitrin <demitri@mit.edu>
2013-10-06 04:23:09 -0700

    updated cgi.py

--- src/cgi-bin/get_account_info_cgi.py
+++ src/cgi-bin/get_account_info_cgi.py
@@ -30,7 +30,7 @@ try:
   print "Content-type:application/json"
   print
   print json.dumps(x)
-except HTTPError as e:
+except:
   print '''HTTP/1.1 401 Unauthorized 
 Content-type:application/json'''
   print
