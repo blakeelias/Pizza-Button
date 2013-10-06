134afad24dcdda50e5bb0fefa5907bef1c9938a2
Will Haack <wmh1993@gmail.com>
2013-10-06 06:57:45 -0400

    started to updated the pizza button onclick

--- src/js/frontend/pizzaButtonOnClick.js
+++ src/js/frontend/pizzaButtonOnClick.js
@@ -2,11 +2,29 @@ define(['cookies/hasCookies', 'cookies/getCookies'],
        function(hasCookies, getCookies) {
         return function() {
 	    if (hasCookies()) {
-		//make an order
+		var accountInfo = getCookies();
+		var ajaxData = {
+		    username: accountInfo.username,
+		    password: accountInfo.password
+		}
+		$.ajax("/cgi-bin/get_account_info_cgi.py", {
+		    type:           "GET",
+		    data:           ajaxData,
+		    contentType:    "application/json;charset=utf-8",
+		    success:        function(jqXHR) {
+                    var data = jqXHR;
+                },
+		    error:          function(jqXHR, textStatus, errorThrown) {
+			    error("Encounted unexpected error while getting account info. Try creating a new account");
+			}
+		    });
 	    }
 	    else {
 		error("Please login or create an account");
 		$("#dropdown").tabs({active: 0});
+		$('html, body').animate({
+			scrollTop: $("#account-info-error").offset().top
+			    }, 500);
 	    }
         }
     });
