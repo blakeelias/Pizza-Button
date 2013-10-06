310aac2855abb3aec25d8d134dcfdb2170424789
demitrin <demitri@mit.edu>
2013-10-05 20:57:34 -0700

    Added place_user_order_cgi.py

--- src/cgi-bin/get_account_info_cgi.py
+++ src/cgi-bin/get_account_info_cgi.py
@@ -25,9 +25,10 @@ args = cgi.FieldStorage()
 email = args["email"].value
 current_pw = args["current_pw"].value
 
-return  ordrin_api.ordrin_api.get_account_info(email,
+x =   ordrin_api.ordrin_api.get_account_info(email,
     current_pw)
 
+print x
 '''
 Example return:
 {u'em': u'artemisfowl5900@yahoo.com', u'first_name': u'demitri', u'last_name': u'nava', u'pw': u'03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', u'_id': u'5250d585d487f2701e00002e'}
